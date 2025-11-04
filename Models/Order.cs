95cc08a2a3cf8efc5fc7ed6b33e51bfe840b160c
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-11-04 04:45:35 -0400

    Update Order model with CustomerId property

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,6 +6,7 @@ public class Order
 {
     public int OrderId { get; set; }
     public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public int CustomerId { get; set; }
     public Customer? Customer { get; set; }
 
     public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
